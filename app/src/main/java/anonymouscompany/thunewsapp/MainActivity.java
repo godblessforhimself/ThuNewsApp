503c30252812c1b9fca8f8b0e61ef7d93309320f
godblessforhimself <jintaonihao2007@163.com>
2017-09-08 20:36:52 +0800

    Initial commit

---
+++ app/src/main/java/anonymouscompany/thunewsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package anonymouscompany.thunewsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
