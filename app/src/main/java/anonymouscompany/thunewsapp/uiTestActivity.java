e1a45c145b7a396850774e7fdd1484e071b26d02
godblessforhimself <jintaonihao2007@163.com>
2017-09-09 20:34:23 +0800

    UI Test

--- app/src/main/java/anonymouscompany/thunewsapp/uiTestActivity.java
+++ app/src/main/java/anonymouscompany/thunewsapp/uiTestActivity.java
@@ -1,14 +1,183 @@
 package anonymouscompany.thunewsapp;
+
+
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.daimajia.swipe.SimpleSwipeListener;
+import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.swipe.adapters.RecyclerSwipeAdapter;
+import com.daimajia.swipe.util.Attributes;
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import jp.wasabeef.recyclerview.animators.FadeInLeftAnimator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class uiTestActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+
+    private ArrayList<String> mDataSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ui_test);
+        setContentView(R.layout.swipe);
         setTitle("uiTestActivity");
+        SwipeLayout swipeLayout =  (SwipeLayout)findViewById(R.id.swipe0);
+
+//set show mode.
+        swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
+
+//add drag edge.(If the BottomView has 'layout_gravity' attribute, this line is unnecessary)
+        swipeLayout.addDrag(SwipeLayout.DragEdge.Left, findViewById(R.id.bottom_wap));
+
+        swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onClose(SwipeLayout layout) {
+                //when the SurfaceView totally cover the BottomView.
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+                //you are swiping.
+            }
+
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+                //when the BottomView totally show.
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+                //when user's hand released.
+            }
+        });
+        // Layout Managers:
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Item Decorator:
+        //recyclerView.addItemDecoration(new DividerItemDecoration(R.drawable.divider));
+        recyclerView.setItemAnimator(new FadeInLeftAnimator());
+
+        // Adapter:
+        String[] adapterData = new String[]{"Alabama", "Alaska", "Arizona", "Arkansas", "California",
+                "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois",
+                "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
+                "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
+                "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon",
+                "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"};
+        mDataSet = new ArrayList<String>(Arrays.asList(adapterData));
+        mAdapter = new RecyclerViewAdapter(this, mDataSet);
+        ((RecyclerViewAdapter) mAdapter).setMode(Attributes.Mode.Single);
+        recyclerView.setAdapter(mAdapter);
+    }
+}
+
+class RecyclerViewAdapter extends RecyclerSwipeAdapter<RecyclerViewAdapter.SimpleViewHolder>
+{
+
+    public static class SimpleViewHolder extends RecyclerView.ViewHolder {
+        SwipeLayout swipeLayout;
+        TextView textViewPos;
+        TextView textViewData;
+        Button buttonDelete;
+
+        public SimpleViewHolder(View itemView) {
+            super(itemView);
+            swipeLayout = (SwipeLayout) itemView.findViewById(R.id.swipe);
+            textViewPos = (TextView) itemView.findViewById(R.id.position);
+            textViewData = (TextView) itemView.findViewById(R.id.text_data);
+            buttonDelete = (Button) itemView.findViewById(R.id.delete);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d(getClass().getSimpleName(), "onItemSelected: " + textViewData.getText().toString());
+                    Toast.makeText(view.getContext(), "onItemSelected: " + textViewData.getText().toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private Context mContext;
+    private ArrayList<String> mDataset;
+
+    //protected SwipeItemRecyclerMangerImpl mItemManger = new SwipeItemRecyclerMangerImpl(this);
+
+    public RecyclerViewAdapter(Context context, ArrayList<String> objects) {
+        this.mContext = context;
+        this.mDataset = objects;
+    }
+
+    @Override
+    public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item, parent, false);
+        return new SimpleViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final SimpleViewHolder viewHolder, final int position) {
+        String item = mDataset.get(position);
+        viewHolder.swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
+        viewHolder.swipeLayout.addSwipeListener(new SimpleSwipeListener() {
+            @Override
+            public void onOpen(SwipeLayout layout) {
+                YoYo.with(Techniques.Tada).duration(500).delay(100).playOn(layout.findViewById(R.id.trash));
+            }
+        });
+        viewHolder.swipeLayout.setOnDoubleClickListener(new SwipeLayout.DoubleClickListener() {
+            @Override
+            public void onDoubleClick(SwipeLayout layout, boolean surface) {
+                Toast.makeText(mContext, "DoubleClick", Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewHolder.buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mItemManger.removeShownLayouts(viewHolder.swipeLayout);
+                mDataset.remove(position);
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position, mDataset.size());
+                mItemManger.closeAllItems();
+                Toast.makeText(view.getContext(), "Deleted " + viewHolder.textViewData.getText().toString() + "!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewHolder.textViewPos.setText((position + 1) + ".");
+        viewHolder.textViewData.setText(item);
+        mItemManger.bindView(viewHolder.itemView, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    @Override
+    public int getSwipeLayoutResourceId(int position) {
+        return R.id.swipe;
     }
 }
