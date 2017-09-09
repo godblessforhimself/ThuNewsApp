4f6c15c51b13645fdaa619a9c53f2e218b07624a
godblessforhimself <jintaonihao2007@163.com>
2017-09-09 15:19:11 +0800

    new

---
+++ app/src/main/java/anonymouscompany/thunewsapp/uiTestActivity.java
@@ -0,0 +1,14 @@
+package anonymouscompany.thunewsapp;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class uiTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui_test);
+        setTitle("uiTestActivity");
+    }
+}
