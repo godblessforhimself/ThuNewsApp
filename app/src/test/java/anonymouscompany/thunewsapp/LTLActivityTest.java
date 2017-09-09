ace94dfd138491ce890e59281d73e9bdc424f082
LTL <onceltl@gamil.com>
2017-09-09 10:45:20 +0800

    robolectric

--- app/src/test/java/anonymouscompany/thunewsapp/LTLActivityTest.java
+++ app/src/test/java/anonymouscompany/thunewsapp/LTLActivityTest.java
@@ -1,9 +1,12 @@
 package anonymouscompany.thunewsapp;
 
 import android.content.res.AssetManager;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
@@ -11,31 +14,27 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * Created by MacheNike on 2017/9/9.
+ * Created by LTL on 2017/9/9.
  */
 
-@RunWith(RobolectricGradleTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class LTLActivityTest
 {
 
     @Test
-    public void testJson(){
-        String str = null;
-        str = RuntimeEnvironment.application.getResources().getString(R.string.app_name);
-
-        AssetManager am = null;
-        am = RuntimeEnvironment.application.getAssets();
-        String strData = null;
+    public void testJson()
+    {
+        MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+        BackendInterface inter=new BackendInter();
         try {
-            InputStream inputStream = am.open("json01.txt");
-            byte buf[] = new byte[1024];
-            inputStream.read(buf);
-            strData = new String(buf);
-            strData =strData.trim();
-            strData.trim();
-        } catch (IOException e) {
-
+            NewsTitle title = inter.getNewsTitle(1, 10, mainActivity);
+            System.out.println(title.list.get(0).news_ID);
+        }catch (Exception e)
+        {
+            System.out.print("No\n");
         }
-       }
+
+        Assert.assertEquals(1,1);
+    }
 }
