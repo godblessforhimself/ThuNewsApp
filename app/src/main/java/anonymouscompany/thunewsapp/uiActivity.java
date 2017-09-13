e4dffd3aee0bd834631e52cea49133b17942b3fd
chenqiuhao1997 <haohao0924@126.com>
2017-09-13 12:54:08 +0800

    初始化设置调整

--- app/src/main/java/anonymouscompany/thunewsapp/uiActivity.java
+++ app/src/main/java/anonymouscompany/thunewsapp/uiActivity.java
@@ -111,7 +111,6 @@ public class uiActivity extends AppCompatActivity implements SearchView.OnQueryT
         tabLayout = (TabLayout) findViewById(R.id.headTab);
         mAdapter = new HomeAdapter();
         recyclerView.setAdapter(mAdapter);
-        news.setPicturesDisplay(1, uiActivity.this);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
         recyclerView.setItemAnimator(new FadeInLeftAnimator());
