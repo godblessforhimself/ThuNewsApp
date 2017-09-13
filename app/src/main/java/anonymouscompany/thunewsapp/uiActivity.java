e9746754de91441a77c7a7634b5a81a7b697f5a9
chenqiuhao1997 <haohao0924@126.com>
2017-09-13 23:45:54 +0800

    debug

--- app/src/main/java/anonymouscompany/thunewsapp/uiActivity.java
+++ app/src/main/java/anonymouscompany/thunewsapp/uiActivity.java
@@ -154,8 +154,6 @@ public class uiActivity extends AppCompatActivity implements SearchView.OnQueryT
 
             mAdapter.notifyDataSetChanged();
             showTip("News fetch finish, total num:" + newsNum);
-            suggestlock.tryLock();
-            suggestlock.unlock();
         }
     };
     //所有runnable，非主线程都不能使用showtip等改变ui的函数，替代办法写到handler handleMessage中。
@@ -163,6 +161,9 @@ public class uiActivity extends AppCompatActivity implements SearchView.OnQueryT
     Runnable netWorkTask = new Runnable() {
         @Override
         public synchronized void run() {
+            if (!suggestlock.tryLock()) {
+                return;
+            }
             Message msg = new Message();
             Bundle data = new Bundle();
             List<NewsTitle.MyList> e = new ArrayList<>();
@@ -183,7 +184,7 @@ public class uiActivity extends AppCompatActivity implements SearchView.OnQueryT
                 msg.obj = ex.toString();
                 handler.sendMessage(msg);
             }
-
+            suggestlock.unlock();
         }
     };
     @Override
@@ -267,6 +268,12 @@ public class uiActivity extends AppCompatActivity implements SearchView.OnQueryT
             }
         });
 
+        int d_n = news.getNight(uiActivity.this);
+        if ((d_n != MODE_NIGHT_YES)&&(d_n != MODE_NIGHT_NO)) {
+            d_n = MODE_NIGHT_NO;
+        }
+        news.setNight(d_n, uiActivity.this);
+
         LinearLayout blay3 = (LinearLayout) findViewById(R.id.blay3);
         blay3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -293,33 +300,35 @@ public class uiActivity extends AppCompatActivity implements SearchView.OnQueryT
                     iv.setImageResource(R.drawable.favorite);
                     isfavourites = 0;
                 }
-                if (suggestlock.tryLock()) {
-                    mPullToRefreshView.setRefreshing(true);
+                mPullToRefreshView.setRefreshing(true);
 
-                    new Thread(new Runnable() {
-                        @Override
-                        public synchronized void run() {
-                            refreshoradd = REFRESH;
-                            Message msg = new Message();
-                            Bundle data = new Bundle();
-                            List<NewsTitle.MyList> e = new ArrayList<NewsTitle.MyList>();
-                            try {
-                                suggest.clear();
-                                suggest.addAll(news.likeNewsTitel(uiActivity.this).list);
-                                e.addAll(suggest.subList(0, pageSize));
-                                data.putParcelableArrayList("news", (ArrayList) e);
-                                msg.setData(data);
-                                msg.what = 1;
-                                handler.sendMessage(msg);
-                            } catch (Exception ex) {
-                                Log.d("exception", ex.toString());
-                                msg.what = 0;
-                                msg.obj = ex.toString();
-                                handler.sendMessage(msg);
-                            }
+                new Thread(new Runnable() {
+                    @Override
+                    public synchronized void run() {
+                        if (!suggestlock.tryLock()) {
+                            return;
                         }
-                    }).start();
-                }
+                        refreshoradd = REFRESH;
+                        Message msg = new Message();
+                        Bundle data = new Bundle();
+                        List<NewsTitle.MyList> e = new ArrayList<NewsTitle.MyList>();
+                        try {
+                            suggest.clear();
+                            suggest.addAll(news.likeNewsTitel(uiActivity.this).list);
+                            e.addAll(suggest.subList(0, pageSize));
+                            data.putParcelableArrayList("news", (ArrayList) e);
+                            msg.setData(data);
+                            msg.what = 1;
+                            handler.sendMessage(msg);
+                        } catch (Exception ex) {
+                            Log.d("exception", ex.toString());
+                            msg.what = 0;
+                            msg.obj = ex.toString();
+                            handler.sendMessage(msg);
+                        }
+                        suggestlock.unlock();
+                    }
+                }).start();
             }
         });
 
